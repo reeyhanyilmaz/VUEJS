6607c2a5d42c112e54de4710d9e135911b8ab27a
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2024-01-07 16:05:39 +0300

    binding

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ createApp({
   data() {
     return {
       name: "reyhan",
+      website: "https://reyhanyilmaz.vercel.app/",
+      aTag: '<a href="https://reyhanyilmaz.vercel.app/">Web site a tag gösterimi</a>',
     };
   },
+  methods: {},
 }).mount("#app");
