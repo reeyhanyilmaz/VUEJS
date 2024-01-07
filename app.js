288282ace2ea7909e8eeaa8704c1f2c07da95a1e
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2024-01-07 16:36:24 +0300

    events

--- app.js
+++ app.js
@@ -5,7 +5,31 @@ createApp({
       name: "reyhan",
       website: "https://reyhanyilmaz.vercel.app/",
       aTag: '<a href="https://reyhanyilmaz.vercel.app/">Web site a tag gösterimi</a>',
+      number: 10,
+      X: 0,
+      Y: 0,
     };
   },
-  methods: {},
+  //   function
+  methods: {
+    increase: function () {
+      // this ile data içindeki degiskene erisilir
+      this.number++;
+    },
+    decrease: function () {
+      this.number--;
+    },
+    addTen: function (num) {
+      this.number += num;
+    },
+    extractionTen: function (num) {
+      this.number -= num;
+    },
+    // böylede func yazılır
+    updateCoordinates(event) {
+      console.log("event :>> ", event);
+      this.X = event.offsetX;
+      this.Y = event.offsetY;
+    },
+  },
 }).mount("#app");
